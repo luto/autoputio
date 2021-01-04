a0acff110965ba2682ea3fae291440d7697390d9
luto <m@luto.at>
2021-01-04 19:11:11 +0100

    add argument to pass destination folder id

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ pip install --user -r requirements.txt
 cp -r torrents.example torrents
 python autoputio.py torrents
 ```
+
+Optionally, a destination folder id can be given as the 2nd argument. All
+torrents will be added to that folder.
--- autoputio.py
+++ autoputio.py
@@ -6,17 +6,17 @@ import traceback
 import putiopy
 
 
-def add_torrents(client, folder):
+def add_torrents(client, folder, destination_id):
     for torrent_file in glob.glob(folder + '/*.torrent'):
         print(f'adding {torrent_file}')
         try:
-            client.Transfer.add_torrent(torrent_file)
+            client.Transfer.add_torrent(torrent_file, destination_id)
             os.unlink(torrent_file)
         except:
             traceback.print_exc()
 
 
-def add_magents(client, folder):
+def add_magents(client, folder, destination_id):
     for magnet_file in glob.glob(folder + '/*.magnet'):
         with open(magnet_file) as f:
             url = f.read()
@@ -25,7 +25,7 @@ def add_magents(client, folder):
 
         try:
             try:
-                client.Transfer.add_url(url)
+                client.Transfer.add_url(url, destination_id)
             except putiopy.ClientError as ex:
                 if ex.type == 'Alreadyadded':
                     print("already added")
@@ -52,13 +52,20 @@ def main():
         usage('no source folder given')
 
     try:
+        destination_id = int(sys.argv[2])
+    except IndexError:
+        destination_id = 0
+    except ValueError:
+        usage(f'destination id is not a number but: {sys.argv[2]}')
+
+    try:
         access_token = os.environ['PUTIO_OAUTH_TOKEN']
     except LookupError:
         usage('no access token given')
 
     client = putiopy.Client(access_token)
-    add_torrents(client, folder)
-    add_magents(client, folder)
+    add_torrents(client, folder, destination_id)
+    add_magents(client, folder, destination_id)
 
 
 if __name__ == '__main__':
